3ddb8dcd18b641b0cf44b2d11bbf5c2c5cf1cd9c
francisco soto  flores <soto_flores98@hotmail.com>
2024-09-08 10:16:51 -0500

    app calculadora

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {
+  StatusBar,
+  View,
+} from 'react-native';
+import {CalculatorScreen} from './presentation/screens/CalculatorScreen';
+import { styles } from './config/theme/app-theme';
+
+function App() {
+
+  return (
+    <View style={styles.background}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={"black"}
+          />
+          <CalculatorScreen/>
+    </View>
+  );
+}
+
+export default App;
